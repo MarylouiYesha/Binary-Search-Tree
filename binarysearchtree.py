ade1455dab0df4d33bbba13d86049be3a0d62922
MarylouiYesha <yesha.maryloui@gmail.com>
2023-01-14 14:51:08 +0800

    left and right child

--- binarysearchtree.py
+++ binarysearchtree.py
@@ -7,3 +7,18 @@ class BST:
     def __init__(self):
         self.root = None
 
+    def add(self, current, value):
+        if self.root == None:
+            self.root = Node(value)
+        else:
+            if value < current.value:
+                if current.left == None:
+                    current.left = Node(value)
+                else:
+                    self.add(current.left, value)
+            else:
+                if current.right == None:
+                    current.right = Node(value)
+                else:
+                    self.add(current.right, value)
+
